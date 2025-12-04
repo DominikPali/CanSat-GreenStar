51f74c95d3d4b1c1cee5f8f51e7cb39646984d5b
Dominik <dominik.palikowski@gmail.com>
2025-12-04 20:22:23 +0100

    Update greenStar_baseCode.ino

--- greenStar_baseCode/greenStar_baseCode.ino
+++ greenStar_baseCode/greenStar_baseCode.ino
@@ -1,60 +1,53 @@
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BMP280.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-
-//#define GPS_SERIAL Serial1 
-#define BMP280_I2C_ADDRESS 0x76
+#include <CanSatKit.h>
 #define ONE_WIRE_BUS 2
-
-Adafruit_BMP280 bmp;  // global BMP280 object
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature sensors(&oneWire);
-
+ 
+ 
+using namespace CanSatKit;
+BMP280 bmp;
+// Pin where the DS18B20 data line is connected
+  OneWire oneWire(ONE_WIRE_BUS);
+  DallasTemperature sensors(&oneWire);
+ 
+ 
+ 
 class GreenStar {
+ 
 public:
+ 
   double temperatureIn, temperatureOut;
   double pressure;
-
+  float latitude, longitude, altitude;
+  bool descent = false;
+ 
+ 
   GreenStar() {
-    // Do NOT do hardware stuff here
-    temperatureIn = 0;
-    temperatureOut = 0;
-    pressure = 0;
-  }
-
-  // Call this from setup()
-  void begin() {
-    Serial.println("START");
+    SerialUSB.println("START ");
+    SerialUSB.begin(9600);
     InitializePressureSensor();
-    sensors.begin();
   }
-
-  void readTemperature() {
+ 
+  void readTemperature(){
     sensors.requestTemperatures();
     temperatureOut = sensors.getTempCByIndex(0);
   }
-
-  void readPressure() {
-    temperatureIn = bmp.readTemperature();
-    pressure = bmp.readPressure() / 100.0; // hPa
+ 
+  void readPressure(){
+    bmp.measureTemperatureAndPressure(temperatureIn, pressure);
+ 
   }
-
-  void saveDataToSD() {
-    Serial.print("Pressure: ");
-    Serial.println(pressure, 2);
-    Serial.print("Temp_in (BMP): ");
-    Serial.println(temperatureIn, 2);
-    Serial.print("Temp_out (OneWire): ");
-    Serial.println(temperatureOut, 2);
-    Serial.println("---");
-  }
-
+ 
   void readGPS(){
  
   }
-
+ 
+  void saveDataToSD() {
+    SerialUSB.println(pressure, 2);
+    SerialUSB.println(temperatureOut, 2);
+    SerialUSB.println(temperatureIn, 2);
+  }
+ 
   void sendDataToGround(){
  
   }
@@ -66,47 +59,46 @@ public:
   void checkForDesiredAltitude(){
  
   }
-
+ 
+ 
 private:
-  void InitializePressureSensor() {
-    Serial.println(F("BMP280 + Arduino Mega test"));
-
-    if (!bmp.begin(BMP280_I2C_ADDRESS)) {
-      Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
-      while (1) {
-        delay(10);
+ 
+ 
+  void InitializePressureSensor()
+  {
+      if(!bmp.begin()) {
+        // if connection failed - print message to the user
+        SerialUSB.println("BMP init failed!");
+        // the program will be 'halted' here and nothing will happen till restart
+        while(1);
+      } else {
+        // print message to the user if everything is OK
+        SerialUSB.println("BMP init success!");
       }
-    }
-
-    bmp.setSampling(
-      Adafruit_BMP280::MODE_NORMAL,
-      Adafruit_BMP280::SAMPLING_X2,    // temperature
-      Adafruit_BMP280::SAMPLING_X16,   // pressure
-      Adafruit_BMP280::FILTER_X16,
-      Adafruit_BMP280::STANDBY_MS_500
-    );
-
-    Serial.println(F("BMP280 initialized."));
+    // setOversampling function allows to set resolution of the pressure sensor
+    // possible values of setOversampling:
+    //  1 -- 2.62 Pa
+    //  2 -- 1.31 Pa
+    //  4 -- 0.66 Pa
+    //  8 -- 0.33 Pa
+    // 16 -- 0.16 Pa
+    bmp.setOversampling(16);
+ 
+    sensors.begin();
   }
 };
-
+ 
+ 
 GreenStar cansat;
-
+ 
+ 
 void setup() {
-  Serial.begin(9600);
-  // For boards with native USB (e.g. Leonardo, Micro, etc.)
-  while (!Serial) {
-    ; // wait for serial port to connect
-  }
-
-  Wire.begin();       // Good practice for I2C
-  delay(100);         // Small delay
-  cansat.begin();     // Now it is safe to initialize sensors
 }
-
+ 
 void loop() {
   cansat.readTemperature();
   cansat.readPressure();
   cansat.saveDataToSD();
+ 
   delay(1000);
 }
